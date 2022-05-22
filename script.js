3dc2c1ce909c3169ea74ac1a891e1ea648e8be9d
Moazzam Moriani <mozmoriani@gmail.com>
2022-05-22 20:04:49 +0500

    Make index page and generic container class responsive

---
+++ script.js
@@ -0,0 +1,15 @@
+mediaMaxWidth750px = window.matchMedia("(max-width: 750px)");
+
+function makeHeroContainer() {
+    let screenWidthLessThan750px = mediaMaxWidth750px.matches;
+    let hero = document.querySelector(".hero-container")
+
+    if (screenWidthLessThan750px) {
+        hero.classList.add("container");
+    } else {
+        hero.classList.remove("container");
+    }
+}
+
+mediaMaxWidth750px.addListener(makeHeroContainer);
+
